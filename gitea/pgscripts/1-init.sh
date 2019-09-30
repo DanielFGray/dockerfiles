08a486cb876a551fd1bea3bf9452bb2f815aeb1f
DanielFGray <DanielFGray@gmail.com>
2019-09-30 06:46:33 +0000

    :hatching_chick:

---
+++ gitea/pgscripts/1-init.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+	CREATE USER gitea;
+	CREATE DATABASE gitea;
+	GRANT ALL PRIVILEGES ON DATABASE gitea TO gitea;
+
+	CREATE USER drone;
+	CREATE DATABASE drone;
+	GRANT ALL PRIVILEGES ON DATABASE drone TO drone;
+EOSQL
+
